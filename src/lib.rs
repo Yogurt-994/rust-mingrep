94048af0e0ecf29aede6d248c4d2ec13fc5ec803
Yogurt <76460511+yogurt-wang@users.noreply.github.com>
2023-05-19 20:08:10 +0800

    在search函数实现中修改为使用迭代器适配器

--- src/lib.rs
+++ src/lib.rs
@@ -55,15 +55,19 @@ pub fn run(config: Config) -> Result<(),Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
